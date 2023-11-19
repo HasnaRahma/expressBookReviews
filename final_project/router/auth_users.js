3531ad10c021fc460a410deb330986f8c71b0c70
HasnaRahma <eh_bedla@esi.dz>
2023-11-19 11:24:34 -0500

    Finish Tasks up to 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,12 +5,34 @@ const regd_users = express.Router();
 
 let users = [];
 
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    // See first if it doesn't exist already in the DataBase
+    if(!doesExist(username))
+        return true;
+    else 
+        return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 //only registered users can login
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,75 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if(req.body.isbn)
+    {
+        const isbn = req.body.isbn;
+        res.send(books[isbn])
+    }    
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if(req.body.author)
+    {
+        const authorName = req.body.author;
+        let matches = []
+        for (const [key, book] of Object.entries(books)) {
+            if (book.author === authorName) {
+              matches.push({ id: key, book: book })
+            }
+          }
+          (matches.length !==0) ? res.send(matches) :
+           res.send("No matching book found !"); // Return null if no matching book is found
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if(req.body.title)
+    {
+        const title = req.body.title;
+        let matches = []
+        for (const [key, book] of Object.entries(books)) {
+            if (book.title === title) {
+              matches.push({ id: key, book: book })
+            }
+          }
+          (matches.length !==0) ? res.send(matches) :
+           res.send("No matching book found !"); // Return null if no matching book is found
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if(req.body.isbn)
+    {
+        const isbn = req.body.isbn;
+        res.send({reviews:books[isbn].reviews})
+    }else {
+        res.send("Please provide an ISBN !")
+    }
 });
 
 module.exports.general = public_users;
